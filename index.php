5e96be1ecf81c2a875a9abf4245129773d815bb3
Abhishek Sharma <sonupapa1999@gmail.com>
2023-04-10 12:34:23 +0530

    final changes

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
+<pre>
+
 <?php
-  
-// Use ls command to shell_exec
-// function
-$output = shell_exec('ifconfig');
-  
-// Display the list of all file
-// and directory
-echo "<pre>$output</pre>";
+
+print `/usr/sbin/ifconfig`;
 ?>
+
+</pre>
+
+
+
+
+
